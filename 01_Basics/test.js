89d562ff7ebc39308fc976b31252af858ec99bec
MisterBisht <bishtmridul520@gmail.com>
2026-06-05 10:57:04 +0530

    changes in test.js

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,3 +1,9 @@
 console.log("hello duniya");
 const EmployeeID = 208 ;
 console.log("employee id is ", EmployeeID);
+//EmployeeID = 112; // this is not possible since we have given EmployeeID as const 
+let Address = "Nainital";
+Address = "Greater Noida"; // in let same variable changes // agar change karna hai toh wapis let mat likhna warna error aayega 
+let mobile_no = 9981661571;
+console.table({EmployeeID,Address,mobile_no});
+//we don't use var nowadays since it has no block scope 
