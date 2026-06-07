32e907febfba59e7015f8608b2988f538fe83ebf
MisterBisht <bishtmridul520@gmail.com>
2026-06-07 15:46:44 +0530

    updated progress

---
+++ 01_Basics/03Conversion_of_datatype.js
@@ -0,0 +1,74 @@
+let score = "33" ;
+// console.log(typeof score);
+
+let valueinNumber = Number(score);
+// console.log(typeof valueinNumber); //number
+
+//if these strings/boolean  were converted to number
+//their output 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Mridul;"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+
+// 1 => true ; 0 => false (for num to bool)
+//"" => false  ; "Mridul" => true (for string to bool)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber , typeof stringNumber) // num to String
+
+//***********Operations**********************************
+
+let value = 3 
+let negvalue = - value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Mridul"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2); // 12 
+// console.log(1 + "2"); // 12 
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3); //2 // precedence of operation padh liyo
+
+// console.log(+true); // 1
+// console.log(+"");// 0   / / these are not a good practice
+
+let num1, num2, num3 ;
+num1 = num2 = num3 = 2 + 2
+// console.log(num1,num2,num3);
+
+//prefix
+let x = 5;
+// console.log(++x); //6
+
+//postfix
+let x = 5;
+
+// console.log(x++);//5
+// console.log(x);//6
+
+
+
+
+
+
+
